59a910a0c4e8aa14000698927cca7ee89032685e
Brittany Barnett <79549588+barnettbrittanym@users.noreply.github.com>
2021-07-09 15:48:07 -0400

    Adding files for calling methods assignment

---
+++ CallingMethods/CallingMethods/Operator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Operator
+    {
+        //Creating a method to perform addition on the two user input numbers
+        public int AddNumbers(int userNumber, int secondNumber)
+        {
+            return userNumber + secondNumber;
+           
+        }
+        //Creating a method to square the first number
+        public int SquareNumber(int userNumber)
+        {
+            return userNumber * userNumber;
+        }
+        //Creating a method to Divide the two numbers
+        public int DivideNumbers(int userNumber, int secondNumber)
+        {
+            return userNumber / secondNumber;
+        }
+
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Taking user input to store variables for math operations
+            Console.WriteLine("Please enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Instantiating the class to call the functions
+            Operator op = new Operator();
+
+            //Calling functions and printing the results to the console
+           int resultSquare = op.SquareNumber(userNumber);
+            Console.WriteLine("The square of the first number is:" + resultSquare);
+
+            int resultAdd = op.AddNumbers(userNumber, secondNumber);            
+            Console.WriteLine("The sum of " + userNumber + " and " + secondNumber + " is " + resultAdd);
+
+            int resultdiv = op.DivideNumbers(userNumber, secondNumber);
+            Console.WriteLine( userNumber + " divided by " + secondNumber + " equals " + resultdiv);
+
+            Console.ReadLine();
+        }
+     }
+}
