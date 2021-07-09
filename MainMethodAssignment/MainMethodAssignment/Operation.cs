40febded44fe316896a47d32581871dd2c6274a6
Brittany Barnett <79549588+barnettbrittanym@users.noreply.github.com>
2021-07-09 17:34:09 -0400

    Adding Method assignment files

---
+++ MainMethodAssignment/MainMethodAssignment/Operation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Operation
+    {
+        //Creating a method math that requires an int passed in
+        public int Math(int num1)
+        {
+            int sum = num1 + 10;
+            return sum;
+        }
+
+        //Creating a method math that requires a decimal input
+        public int Math(decimal num2)
+        {
+            int product =(Convert.ToInt32(num2)) * 10;
+            return product;
+        }
+
+        //Creating a method math that converts a string to an integer and performs a math function
+        public int Math(string num3)
+        {
+            int number = (Convert.ToInt32(num3));
+            int difference = 10 - number;
+            return difference;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instatiating the class Operation
+            Operation op = new Operation();
+
+            //Calling the first method
+            int sum1=op.Math(12);
+            Console.WriteLine("Your number plus 10 is: " + sum1);
+
+            //Calling the second method
+            int sum2 = op.Math(1.492m);
+            Console.WriteLine("Your number multiplied by 10 is:" + sum2);
+
+            //Calling the third method
+            int sum3 = op.Math("5");
+            Console.WriteLine("10 minus your number is:" + sum3);
+
+            Console.ReadLine();
+        }
+    }
+}
