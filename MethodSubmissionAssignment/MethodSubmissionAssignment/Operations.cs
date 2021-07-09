119e0ed0ce165b5dc9510e4e96a7ee3dc2f48934
Brittany Barnett <79549588+barnettbrittanym@users.noreply.github.com>
2021-07-09 18:46:12 -0400

    Comment code

--- MethodSubmissionAssignment/MethodSubmissionAssignment/Operations.cs
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Operations.cs
@@ -8,6 +8,7 @@ namespace MethodSubmissionAssignment
 {
     class Operations
     {
+        //creating a method called math with an optional second parameter
         public int Math(int numberOne, int numberTwo=10)
         {
             int result = numberOne * numberTwo;
--- MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -10,20 +10,25 @@ namespace MethodSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            //Asking for a user generated integer to pass into the method
             Console.WriteLine("Please enter an integer value:");
             int numberOne = Convert.ToInt32(Console.ReadLine());
 
+            //Creating a prompt to allow the user to enter an optional second parameter
             Console.WriteLine("You may enter a second integer value now to perform a mathematical operation: (This number is optional)");
             string response = Console.ReadLine();
 
+            //using an if else statement to pass in the proper parameters based on the user input
             if (response == "")
             {
+                //instantiating the class and calling the method using one parameter
                 Operations op = new Operations();
                 int num3 = op.Math(numberOne);
                 Console.WriteLine(numberOne + " multiplied by 10 equals " + num3);
             }
             else
             {
+                //instantiating the class and calling the method using two user generated parameters
                 int numberTwo = Convert.ToInt32(response);
                 Operations op = new Operations();
                 int num3 = op.Math(numberOne, numberTwo);
