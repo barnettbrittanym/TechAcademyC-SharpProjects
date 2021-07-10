0c6b03e9e82ea132940a2e809b9c0a6b3878af37
Brittany Barnett <79549588+barnettbrittanym@users.noreply.github.com>
2021-07-09 22:58:08 -0400

    Add files for the Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/Operator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Operator
+    {
+        //Creating a void method called Numbers
+        public void Numbers(int num1 = 4, int num2=20)
+        {
+            //Performing a math operation with the first parameter
+            int result = num1 / 2;
+            Console.WriteLine(num1 + " divided by 2 is: " + result);
+            
+            //Writing the value of the second parameter to the console
+            Console.WriteLine("The second number passed into this method is: {0}", num2);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Instantiating the operator class
+            Operator z = new Operator();
+
+            //Calling the numbers method by passing in two values
+            z.Numbers(2,5);
+
+            //Calling the numbers method by specifying the parameters by name
+            z.Numbers();
+     
+            Console.ReadLine();
+        }   
+    }
+}
