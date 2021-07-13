e04bf85616f357bec84fc445cd21cc226409fa18
Brittany Barnett <79549588+barnettbrittanym@users.noreply.github.com>
2021-07-13 01:45:47 -0400

    Adding files for StructAssignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public struct Number { public decimal Amount; }
+
+namespace StructAssignment
+{
+    
+
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //Instantiating the Number Struct
+            Number num1;
+
+            //Specification of an amount
+            num1.Amount = 1.056543m;
+
+            //Printing the amount set in the struct to the console
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+         
+
+           
+    }
+    }
+}
