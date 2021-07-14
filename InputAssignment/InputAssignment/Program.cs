f9691eae8ae8b2b268c49d2231a2dc7384546336
Brittany Barnett <79549588+barnettbrittanym@users.noreply.github.com>
2021-07-14 17:35:23 -0400

    Adds files for input assignment

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asking the user to input a number
+            Console.WriteLine("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //Using StreamWriter to append the values to the numberLog file
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Brittany Barnett\Logs\numberLog.txt", true))
+            {
+                file.WriteLine(input);
+            }
+
+            //setting a variable list to capture all the values from the file and print them to the console
+            string list = File.ReadAllText(@"C:\Users\Brittany Barnett\Logs\numberLog.txt");
+            Console.WriteLine(list);
+            Console.ReadLine();
+
+        }
+    }
+}
