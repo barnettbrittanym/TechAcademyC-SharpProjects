1711d190c8e01aac26fe0a0744a5e6a3d167824b
Brittany Barnett <79549588+barnettbrittanym@users.noreply.github.com>
2021-07-14 18:16:03 -0400

    Add DateTime Submission files

---
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the current datetime and printing it to the console
+            var currentTime = DateTime.Now;
+            Console.WriteLine("It is currently:" + currentTime);
+
+            //Asking the user to input a number that will be used for hours
+            Console.WriteLine("Please enter a number:");
+            int numberHours = Convert.ToInt32(Console.ReadLine());
+
+            //Returning the future time by adding that number of hours to the current datetime
+            Console.WriteLine("In {0} hours it will be: {1}", numberHours, currentTime.AddHours(numberHours));
+            Console.ReadLine();
+        }
+    }
+}
