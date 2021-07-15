847cdc8c440e1bd8e554530dc7f8dbbcb71c4125
Brittany Barnett <79549588+barnettbrittanym@users.noreply.github.com>
2021-07-14 22:18:00 -0400

    Add files for ConstructorSubmission

---
+++ ConstructorSubmission/ConstructorSubmission/Persons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    public class Persons
+    {
+        //Chaining two constructors together
+        public Persons(string fname, string lname):this(fname, lname, "")
+        {
+
+        }
+
+        public Persons(string fname, string lname, string dept)
+        {
+            string firstName = fname;
+            string lastName = lname;
+            string department = dept;
+            string fullName = (fname + " " + lname);
+            Console.WriteLine(fullName);
+        }
+
+       
+            
+
+    }
+    
+}
---
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a const variable
+            const string banana = "This is a banana string";
+            Console.WriteLine(banana);
+
+            //Creating a variable using the keyword var
+            var temperature = 72;
+            Console.WriteLine(temperature);
+
+            //instantiating a person of the chained class
+            Persons employee = new Persons("Bob", "Jones");
+            
+
+            Console.ReadLine();
+       
+        }
+    }
+}
